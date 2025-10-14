4b0108adff4dddf3fc859f77f006526bc0cdb87a
Jan Ruch <private@jruch.ch>
2025-10-14 14:19:55 +0200

    add manual query to prevent lazy loading

--- policy-management-backend/src/main/java/com/lakesidemutual/policymanagement/infrastructure/PolicyRepository.java
+++ policy-management-backend/src/main/java/com/lakesidemutual/policymanagement/infrastructure/PolicyRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.lakesidemutual.policymanagement.domain.customer.CustomerId;
 import com.lakesidemutual.policymanagement.domain.policy.PolicyAggregateRoot;
 import com.lakesidemutual.policymanagement.domain.policy.PolicyId;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * The PolicyRepository can be used to read and write PolicyAggregateRoot objects from and to the backing database. Spring automatically
@@ -19,5 +20,9 @@ public interface PolicyRepository extends JpaRepository<PolicyAggregateRoot, Pol
 		return PolicyId.random();
 	}
 
+	@Query("SELECT policyAggergateRoot FROM PolicyAggregateRoot policyAggergateRoot " +
+			"LEFT JOIN FETCH policyAggergateRoot.insuringAgreement " +
+			"LEFT JOIN FETCH policyAggergateRoot.insuringAgreement.agreementItems " +
+			"WHERE policyAggergateRoot.customerId = ?1 ORDER BY policyAggergateRoot.creationDate DESC")
 	public List<PolicyAggregateRoot> findAllByCustomerIdOrderByCreationDateDesc(CustomerId customerId);
 }
